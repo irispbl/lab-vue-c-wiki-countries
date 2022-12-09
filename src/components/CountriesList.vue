bb6d62f16a0c7d03be606cb061435b5e339835ca
Iris Pascual <irispblg@gmail.com>
2022-12-09 14:16:17 +0100

    add finish lab

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-5" style="max-height: 90vh; overflow: scroll">
                 <div class="list-group">
-                    <router-link v-for="(country,index) in countries" class="list-group-item list-group-item-action" :to="`/list/${country.alpha3Code}`">
+                    <router-link v-for="(country,index) in countries" class="list-group-item list-group-item-action" :to="getURL(country)">
                         <img :src="'https://flagpedia.net/data/flags/icon/72x54/' + country.alpha2Code.toLowerCase()+ '.png'" />
                         <p>{{country.name.common}}</p>
                     </router-link>
@@ -24,6 +24,11 @@ export default {
     data(){
         return{
             countries: countries
+        }
+    },
+    methods: {
+        getURL(country){
+            return `/list/${country.alpha3Code}`;
         }
     }
 
--- src/components/CountryDetails.vue
+++ src/components/CountryDetails.vue
@@ -1,33 +1,33 @@
 <template>
   <div class="col-7">
     <img
-      src="https://restcountries.eu/data/fra.svg"
+      :src="
+        'https://www.geonames.org/flags/x/' +
+        showCountry.alpha2Code.toLowerCase() +
+        '.gif'
+      "
       alt="country flag"
       style="width: 300px"
     />
-    <h1>France</h1>
+    <h1>{{ showCountry.name.common }}</h1>
     <table class="table">
       <thead></thead>
       <tbody>
         <tr>
           <td style="width: 30%">Capital</td>
-          <td>Paris</td>
+          <td>{{ showCountry.capital[0] }}</td>
         </tr>
         <tr>
           <td>Area</td>
-          <td>551695 km <sup>2</sup></td>
+          <td>{{ showCountry.area }} km <sup>2</sup></td>
         </tr>
         <tr>
           <td>Borders</td>
           <td>
-            <ul>
-              <li><a href="/AND">Andorra</a></li>
-              <li><a href="/BEL">Belgium</a></li>
-              <li><a href="/DEU">Germany</a></li>
-              <li><a href="/ITA">Italy</a></li>
-              <li><a href="/MCO">Monaco</a></li>
-              <li><a href="/ESP">Spain</a></li>
-              <li><a href="/CHE">Switzerland</a></li>
+            <ul v-for="(country, index) in showCountry.borders" :key="index">
+              <li>
+                <router-link :to="`/${showCountry.alpha3Code}`">{{country}}</router-link>
+              </li>
             </ul>
           </td>
         </tr>
@@ -45,13 +45,16 @@ export default {
     };
   },
   computed: {
-    country() {
-      this.$route.params.alpha3Code === country.alpha3Code;
+    showCountry() {
+      const countryId = this.$route.params.alpha3Code;
+      return this.countries.find(function (country) {
+        if (country.alpha3Code === countryId) {
+          return true;
+        } else {
+          return false;
+        }
+      });
     },
-  },
-  mounted() {
-    // NO se tiene que hacer en mounted
-    console.log(this.$route.params.alpha3Code);
   },
 };
 </script>
